e5063f79c6b9c922f09bd037f29ef564628689f8
johnny <johnnyjohnny6201@gmail.com>
2020-03-30 02:14:17 +0800

    4_3

--- 4_3_uLCD/main.cpp
+++ 4_3_uLCD/main.cpp
@@ -6,5 +6,10 @@ uLCD_4DGL uLCD(D1, D0, D2);
 int main()
 {
       uLCD.printf("\nHello uLCD World\n"); //Default Green on black text
-      wait(30);
+      wait(3);
+      uLCD.reset();
+      uLCD.cls();
+      wait(3);
+      uLCD.background_color(0x0000FF);
+      wait(3);
 }
